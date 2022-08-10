f6c447a7b26d19590328d01b910f63aae86216d5
Tyler Meador <meador.tyler0@gmail.com>
2022-08-10 00:48:02 -0500

    Finished up demo

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 255, 255, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 1);
+    [SerializeField] float destructionDelay = 0.5f;
 
-    void OnCollisionEnter2D(Collision2D other) {
+    bool hasPackage = false;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
         Debug.Log("Ouch!");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package") {
-
-            hasPackage = true;
-            Debug.Log("Package picked up!");
-
-        } else if (other.tag == "Customer") {
-
-            if (hasPackage) {
-
+        if (other.tag == "Package")
+        {
+            if(!hasPackage)
+            {
+                hasPackage = true;
+                Debug.Log("Package picked up!");
+                spriteRenderer.color = hasPackageColor;
+                Destroy(other.gameObject, destructionDelay);
+            }
+            else
+            {
+                Debug.Log("You already have a package!");
+            }
+        }
+        else if (other.tag == "Customer")
+        {
+            if (hasPackage)
+            {
                 hasPackage = false;
                 Debug.Log("Package delivered!");
-
-            } else {
-
+                spriteRenderer.color = noPackageColor;
+                Destroy(other.gameObject, destructionDelay);
+            }
+            else
+            {
                 Debug.Log("Get the package first!");
-
             }
         }
     }
